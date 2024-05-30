64230ea6dab69db6d14cee12d181701d77d3af7d
Peter Pfeufer <ppf@qbus.de>
2024-05-30 08:53:58 +0200

    [CHANGE] Don't include modified date and author
    Libs modified by `strauss` had the author and date includes in their PHPdoc header. This caused a lot of noise in Git, so we don't do that anymore.

--- Sources/Libs/yahnis-elsts/plugin-update-checker/load-v5p4.php
+++ Sources/Libs/yahnis-elsts/plugin-update-checker/load-v5p4.php
@@ -2,7 +2,7 @@
 /**
  * @license MIT
  *
- * Modified by Peter Pfeufer on 13-April-2024 using {@see https://github.com/BrianHenryIE/strauss}.
+ * Modified using {@see https://github.com/BrianHenryIE/strauss}.
  */
 
 namespace WordPress\Ppfeufer\Plugin\WpMemoryUsage\Libs\YahnisElsts\PluginUpdateChecker\v5p4;
